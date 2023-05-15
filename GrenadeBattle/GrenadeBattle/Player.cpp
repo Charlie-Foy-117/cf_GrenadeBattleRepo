134a603fc47f6b99ab2ff5e35fd73294612a014f
Charlie-Foy-117 <charliefoy03@gmail.com>
2023-05-15 16:00:51 +0100

    Player can move again

--- GrenadeBattle/GrenadeBattle/Player.cpp
+++ GrenadeBattle/GrenadeBattle/Player.cpp
@@ -1,6 +1,12 @@
 #include "Player.h"
 #include "AssetManager.h"
 
+enum class PhysicsType
+{
+	SYMPLECTIC_EULER,
+	VELOCITY_VERLET
+};
+
 Player::Player()
 	: PhysicsObject()
 	, pips()
@@ -22,7 +28,59 @@ Player::Player()
 
 void Player::Update(sf::Time frameTime)
 {
-	PhysicsObject::Update(frameTime);
+	//Practical Task - Physics Alternatives
+	//
+	//
+	const float DRAG = 10.0f;
+	const PhysicsType physics = PhysicsType::VELOCITY_VERLET;
+	sf::Vector2f lastFramePos = GetPosition();
+	sf::Vector2f halfFrameVelocity;
+
+	switch (physics)
+	{
+	case PhysicsType::VELOCITY_VERLET:
+
+		//velocity verlet
+
+		//get the half frame velocity using the previous frame's acceleration
+		halfFrameVelocity = velocity + acceleration * frameTime.asSeconds() / 2.0f;
+
+		//get new frame's position using half frame velocity
+		SetPosition(GetPosition() + halfFrameVelocity * frameTime.asSeconds());
+
+		//update the acceleration
+		UpdateAcceleration();
+
+		//get new frame's velocity using half frame velocity and updated acceleration
+		velocity = halfFrameVelocity + acceleration * frameTime.asSeconds() / 2.0f;
+
+		//calculate the drag
+		velocity = velocity - velocity * DRAG * frameTime.asSeconds();
+
+		break;
+	case PhysicsType::SYMPLECTIC_EULER:
+
+		//semi-implicit / symplectic euler
+
+		//update the velocity to account for acceleration
+		velocity = velocity + acceleration * frameTime.asSeconds();
+
+		//calculate the drag
+		velocity = velocity - velocity * DRAG * frameTime.asSeconds();
+
+		//set the new position using the calculted velocity
+		SetPosition(GetPosition() + velocity * frameTime.asSeconds());
+
+		//update the acceleration
+		UpdateAcceleration();
+
+		break;
+	default:
+		break;
+
+		//two frames ago (for next frame)
+		twoFramesOldPosition = lastFramePos;
+	}
 }
 
 void Player::Draw(sf::RenderTarget& target)
@@ -53,11 +111,13 @@ void Player::HandleCollision(SpriteObject& other)
 	}
 	else
 	{
-		//move in y direction
-		newPosition.y += depth.y;
-		velocity.y = 0;
-		acceleration.y = 0;
-
+		if (velocity.y > 0)
+		{
+			//move in y direction
+			newPosition.y += depth.y;
+			velocity.y = 0;
+			acceleration.y = 0;
+		}
 	}
 }
 
